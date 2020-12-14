a2878c30629bf78644aa7ffd36d5b46444c768ea
Quintonamore <teas98@gmail.com>
2020-12-13 23:34:15 -0800

    Style changes for user select and game select

--- frontend/src/components/GameSelect.js
+++ frontend/src/components/GameSelect.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./GameSelect.css";
 
 function GameSelect(props) {
-    const renderGameList = [];
-    for (let game of props.gameList) {
-        renderGameList.push(<div onClick={() => props.selectGame(game)} key={game.id}>{game.name}</div>);
-    }
-    return <div>{renderGameList}</div>;
+  const renderGameList = [];
+  for (let game of props.gameList) {
+    renderGameList.push(
+      <div onClick={() => props.selectGame(game)} key={game.id}>
+        {game.gameName}
+      </div>
+    );
+  }
+  return <div className="games">{renderGameList}</div>;
 }
 
 export default GameSelect;
--- frontend/src/components/UserSelect.js
+++ frontend/src/components/UserSelect.js
@@ -1,33 +1,18 @@
-import React from 'react';
-import './UserSelect.css';
-
-const users = [
-  "Q",
-  "Erika",
-  "Michael",
-  "Brando n",
-  "Bobbie",
-  "Alix",
-  "Alix 2",
-  "Matthew",
-  "CHLoe",
-  "Elise",
-  "Alan Jackson",
-  "Lefty Frizzel",
-  "Merle Haggard",
-  "Keith Whitley"
-];
+import React from "react";
+import "./UserSelect.css";
 
 class UserSelect extends React.Component {
   render() {
     // Build User List
     const userList = [];
-    users.forEach(user => userList.push(<div key={user} onClick={this.props.giveDrink}>{user}</div>));
-    return (
-      <div className="users">
-        {userList}
-      </div>
+    this.props.users.forEach((user) =>
+      userList.push(
+        <div key={user} onClick={this.props.giveDrink}>
+          {user}
+        </div>
+      )
     );
+    return <div className="users">{userList}</div>;
   }
 }
 
