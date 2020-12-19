8afb3d2c62c76685a683ffc431e41d8590c1f162
Quintonamore <teas98@gmail.com>
2020-12-18 23:34:15 -0800

    Made Users an Array
    Changed them to live in an array for easier management, and so that the
    GamesMap Objects don't get really complicated.

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,32 @@ const crypto = require('crypto');
 // Server init and Games Map init
 const wss = new WebSocket.Server({ port: 8080 });
 const games = new Map();
+const users = [];
+const THRESHOLD = 6400;
+
+function User(name, gameId) {
+  this.name = name;
+  this.gameId = gameId
+}
 
 function generateIdNameList() {
   const idNameList = [];
   for (key of games.keys()) {
     const tempGame = games.get(key);
-    idNameList.push({id: key, ...tempGame});
+    idNameList.push({id: key, ...tempGame, users: users.filter(comp => comp.gameId === key)});
   }
   return idNameList;
 }
 
-
 wss.on('connection', function connection(ws) {
+
+  const connectionUser = new User("", "");
+  users.push(connectionUser);
+  const localIndex = users.length - 1;
+
+  setInterval(() => {
+    if(ws.bufferedAmount < THRESHOLD) sendGames();
+  }, 1000);
 
   /**
    * Expecting a newGameObj JSON with the following properties
@@ -25,9 +39,9 @@ wss.on('connection', function connection(ws) {
     const id = crypto.randomBytes(16).toString("hex");
     gameInfo.createdAt = Date.now();
     gameInfo.countries = [];
-    gameInfo.users = [];
     delete gameInfo.action;
     games.set(id, gameInfo);
+    connectionUser.gameId = id;
     sendGames();
   }
 
@@ -36,7 +50,16 @@ wss.on('connection', function connection(ws) {
   }
 
   function modify(data) {
+    delete data.users;
     games.set(data.id, data);
+  }
+
+  function joinGame(data) {
+    connectionUser.gameId = data.gameId;
+  }
+
+  function updateName(data) {
+    connectionUser.name = data.name;
   }
 
   ws.on('message', function incoming(message) {
@@ -45,17 +68,26 @@ wss.on('connection', function connection(ws) {
     switch (data.action) {
       case 'newGame':
         newGame(data);
-      case 'refresh':
-        sendGames();
+        break;
+      case 'joinGame':
+        joinGame(data);
         break;
       case 'modify':
         modify(data);
+        break;
+      case 'updateName':
+        updateName(data);
+        break;
       default:
-        sendGames();
         break;
     }
+    sendGames();
+  });
+
+  ws.on('close', function clear() {
+    delete users[localIndex];
   });
 
   // On successful connection send server list
-  ws.send(JSON.stringify(generateIdNameList()));
+  sendGames();
 });
